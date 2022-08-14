24d0608061dd5cbb81f4f267b2d1e53e3f4e44fc
danielft2 <dadadadaadad>
2022-08-14 11:13:15 -0300

    cookies

--- js/cookies.js
+++ js/cookies.js
@@ -1,17 +1,32 @@
-function cookiesGA4() {  
-  window.dataLayer = window.dataLayer || [];
-  function gtag() {
-    dataLayer.push(arguments);
+function initCookiesGA4() {
+  const cookies = document.querySelector('.cookies')
+  const btnCookies = document.querySelector('.btn-cookies')
+
+  function cookiesGA4() {  
+    cookies.classList.remove('active');
+  
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+  
+    gtag("js", new Date());
+    gtag("config", "G-PWL1JMM9M6");
+    
+    const USER_ID = Math.floor(Date.now() * Math.random()).toString(36);
+    gtag("set", { user_id: USER_ID });
+    localStorage.setItem('@idsession', USER_ID);
   }
 
-  gtag("js", new Date());
-  
-  const USER_ID = Math.floor(Date.now() * Math.random()).toString(36)
-  
-  gtag("config", "G-PWL1JMM9M6");
-  gtag("set", { user_id: USER_ID });
+  if(localStorage.getItem('@idsession')) {
+    cookies.classList.remove('active');
+  }
 
-  localStorage.setItem('@idsession', USER_ID);
+  btnCookies.addEventListener('click', cookiesGA4);
 }
 
-cookiesGA4();
+initCookiesGA4();
+
+
+
+
